d7ef524225b2b931c372a0f649f3d7f12c3a998e
dzonda <damien.zonda@lambda-coins.com>
2023-05-01 14:18:59 +0200

    update ts configs

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,13 @@ module.exports = {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        include: path.resolve('src'),
+        include: path.resolve('src/client'),
         exclude: path.resolve('node_modules'),
         loader: 'ts-loader',
       },
       {
         test: /\.(js|jsx)$/,
-        include: path.resolve('src'),
+        include: path.resolve('src/client'),
         exclude: path.resolve('node_modules'),
         loader: 'babel-loader',
       },
@@ -37,10 +37,19 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: 'public/favicon.ico', to: './' }],
     }),
+    new webpack.DefinePlugin({
+      'process.env.REACT_APP_SOCKET_ID': JSON.stringify(process.SOCKET_ID),
+      'process.env.REACT_APP_SOCKET_HOST': JSON.stringify(process.env.SOCKET_HOST),
+      'process.env.REACT_APP_SOCKET_PORT': JSON.stringify(process.env.SOCKET_PORT),
+      'process.env.REACT_APP_SOCKET_NAMESPACE': JSON.stringify(process.env.SOCKET_NAMESPACE),
+    }),
   ],
   resolve: {
     modules: ['node_modules'],
     extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@/client': path.resolve(__dirname, 'src/client'),
+    },
   },
   node: {
     global: true,
