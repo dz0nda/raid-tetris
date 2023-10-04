e0c3ff8152546d4684db1be394707c58c13de785
dz0nda <dzonda@protonmail.com>
2023-10-04 19:39:37 +0200

    update index

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
 // import 'regenerator-runtime/runtime';
 // import Server from './server';
-import { RedTetris } from './RedTetris';
+import { RedTetris } from './app';
 
 new RedTetris('0.0.0.0', 3000).listen();
