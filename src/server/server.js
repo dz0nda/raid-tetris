5d092e48ad19f813f76e4a6c27ac9424465e6795
dzonda <damien.zonda@lambda-coins.com>
2023-10-28 21:24:10 +0200

    add host listening

--- src/server/server.js
+++ src/server/server.js
@@ -5,5 +5,5 @@ import server from './index';
 const { host, port } = params.server;
 
 server.listen({ host, port }, () => {
-  logger.info(`Listening on port ${port}!`);
+  logger.info(`Listening on http:${host}:${port}!`);
 });
